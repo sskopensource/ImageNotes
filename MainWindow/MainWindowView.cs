e4038e93822abefa0e8a478e6dfd7236e689cfb1
SSK Opensource <opensource.ssk@gmail.com>
2022-04-28 20:55:07 +0530

    Initial version of files.

--- MainWindow/MainWindowView.cs
+++ MainWindow/MainWindowView.cs
@@ -5,27 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace MainWindow
 {
     public class MainWindowView:MainWindowUi
     {
-        
-
         public TextBox label;
-
+        public TextBlock label2;
         public MainWindowView()
         {
-            label = new TextBox();
-            label.MinWidth = 20;
-            label.Height = 40;
-            label.FontSize = 30;
-            Thickness x = new Thickness() { Left = 20, Top = 20 };
-            label.Margin = x;
-            labelRegion.Children.Add(label);
-            this.labelRegion.Background = Brushes.Yellow;
-            this.WindowState = WindowState.Maximized;
             ShowDialog();
         }
     }
